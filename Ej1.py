29bf018ca14863d4bea6295b7d3b5dab0115c333
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-20 22:13:14 -0600

    Validación de datos

--- Ej1.py
+++ Ej1.py
@@ -11,12 +11,13 @@ while True:
     letters = 0
     for i in dpi:
         if not i.isdigit():
-            print('El DPI solo debe contener números.')
             letters += 1
     if len(dpi) != 13:
         print('El DPI debe tener 13 números.')
     elif letters != 0:
         print('Solo debe contener números.')
+    else:
+        break
 
 department = input('Ingrese su departamento de origen: ')
 birth_year = int(input('Ingrese su año de nacimiento: '))
@@ -27,3 +28,7 @@ voting_age = 18
 if department == 'Alta Verapaz' or department == 'Peten':
     voting_age = 17
 
+if 2025 - birth_year <= voting_age:
+    print('Votante no válido.')
+else:
+    print(f'Bienvenido {name}, su centro de votación está en {department}.')
