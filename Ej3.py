aef315e807a801da59bd0ce52a0237a1fc5054b1
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-20 23:13:47 -0600

    Corrección de errores de inicio de sesión

--- Ej3.py
+++ Ej3.py
@@ -8,19 +8,32 @@ while attempts > 0:
     user = input('Ingrese su usuario: ')
     password = input('Ingrese su contraseña: ')
 
-    counter = 0
+    user_num = 0
     for i in users:
-        if user != i['user']:
-            counter += 1
-    if counter == 3:
+        if user == i['user']:
+            break
+        else:
+            user_num += 1
+
+    if user_num == 3:
         attempts -= 1
         print(f'No existe el usuario. Intentos restantes: {attempts}\n')
         continue
 
-    if password != users[counter]['password']:
+    if password != users[user_num]['password']:
         attempts -= 1
         print(f'Contraseña incorrecta. Intentos restantes: {attempts}\n')
         continue
+    else:
+        print('INICIO DE SESIÓN EXITOSO\n')
+        break
 
 if attempts == 0:
     print('ACCESO BLOQUEADO')
+else:
+    while True:
+        print('Opciones: \n1. Ver perfil. \n2. Cambiar contraseña. \n3. Cerrar sesión.')
+        option = int(input('Ingrese una opción: '))
+
+        if option == 1:
+            print(f'Tu usuario es: {users[user_num]['user']}')
